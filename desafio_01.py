00bb7d15ebdd4030630f538625503dcb1dec3a7f
Euzébio OLivertino <olivertinoborges.euzebio@gmail.com>
2024-06-10 21:25:07 -0300

    inclusos arquivos pendentes de commit

---
+++ desafio_01.py
@@ -0,0 +1,30 @@
+
+###Crie um script Python que leia o nome de uma pessoa e mostre uma menságem de boas vindas de acordo com o valor digitado.
+
+print ('===== DESAFIO 01 =====')
+nome = input('Qual o seu nome?  ')
+print ('Olá', nome, '! Prazer em te conhecer!')
+
+###Crie um script Python que leia o dia, o mês, e o ano de nascimento de uma pessoa e mostre a mensagem com a data formatada.
+
+print ('===== DESAFIO 02 =====')
+dia = input('Qual o dia de seu nascimento? ')
+mês = input('Qual o mês de seu nascimento? ')
+ano = input('Qual é o ano de seu nascimento? ')
+print('A data de seu aniversário é', dia,'/', mês,'/', ano, '!')
+
+###Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+print ('===== DESAFIO 03 =====')
+n1 = input('Digite o primeiro numero: ')
+n2 = input ('Digite o segundo numero: ')
+print ('A soma entre o Primeiro e o Segundo numero é:', int(n1) + int(n2))
+
+       
+
+
+
+
+
+
+
---
+++ teste_01.py
@@ -0,0 +1,10 @@
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+cidade = input('Qual sua cidade?')
+print (nome, idade, peso, cidade)
+
+
+
+
+               
